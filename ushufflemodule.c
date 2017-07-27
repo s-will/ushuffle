6b613ac6e3a6741766cf33b8f4cdff78bdb8cd02
Sebastian Will <swill@csail.mit.edu>
2017-07-27 11:51:12 +0200

    Extend python wrapper to support python 2 & 3

--- ushufflemodule.c
+++ ushufflemodule.c
@@ -2,20 +2,31 @@
 
 #include "ushuffle.h"
 
-static int l_ = 0;
+struct module_state {
+    int l_;
+};
+
+#if PY_MAJOR_VERSION >= 3
+#define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
+#else
+#define GETSTATE(m) (&_state)
+static struct module_state _state;
+#endif
 
 static PyObject *ushuffle_shuffle1(PyObject *self, PyObject *args) {
 	const char *s;
 	const int l;
 	const int k;
 
+        struct module_state *st = GETSTATE(self);
+
 	/* example python call: ushuffle.shuffle1('string', 6, 2) */
 	if (!PyArg_ParseTuple(args, "sii", &s, &l, &k)) {
-		l_ = 0;
+		st->l_ = 0;
 		return NULL;
 	}
 
-	l_ = l;
+	st->l_ = l;
 	shuffle1(s, l, k);
 	Py_INCREF(Py_None);
 	return Py_None;
@@ -25,14 +36,16 @@ static PyObject *ushuffle_shuffle2(PyObject *self, PyObject *args) {
 	char *t;
 	PyObject *T;
 
-	if (l_ <= 0)
+        struct module_state *st = GETSTATE(self);
+
+	if (st->l_ <= 0)
 		return NULL;
 
-	if ((t = malloc((l_ + 1) * sizeof(char))) == NULL)
+	if ((t = malloc((st->l_ + 1) * sizeof(char))) == NULL)
 		return NULL;
 
 	shuffle2(t);
-	t[l_] = '\0';
+	t[st->l_] = '\0';
 	T = Py_BuildValue("s", t);
 	free(t);
 	return T;
@@ -45,15 +58,17 @@ static PyObject *ushuffle_shuffle(PyObject *self, PyObject *args) {
 	char *t;
 	PyObject *T;
 
+        struct module_state *st = GETSTATE(self);
+
 	/* example python call: ushuffle.shuffle('string', 6, 2) */
 	if (!PyArg_ParseTuple(args, "sii", &s, &l, &k))
 		return NULL;
-	l_ = l;
-	if ((t = malloc((l_ + 1) * sizeof(char))) == NULL)
+	st->l_ = l;
+	if ((t = malloc((st->l_ + 1) * sizeof(char))) == NULL)
 		return NULL;
 
 	shuffle(s, t, l, k);
-	t[l_] = '\0';
+	t[st->l_] = '\0';
 	T = Py_BuildValue("s", t);
 	free(t);
 	return T;
@@ -69,6 +84,25 @@ static PyMethodDef ushuffleMethods[] = {
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
-PyMODINIT_FUNC initushuffle(void) {
-  (void) Py_InitModule("ushuffle", ushuffleMethods);
+
+#if PY_MAJOR_VERSION >= 3
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "ushuffle",
+        NULL,
+        sizeof(struct module_state),
+        ushuffleMethods,
+};
+
+PyMODINIT_FUNC
+PyInit_ushuffle(void) {
+    return
+        PyModule_Create(&moduledef);
 }
+#else
+PyMODINIT_FUNC initushuffle(void) {
+    return
+        Py_InitModule("ushuffle", ushuffleMethods);
+}
+#endif
